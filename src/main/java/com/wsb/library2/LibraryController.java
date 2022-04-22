6bbffbf38d90aaf87e4fc5ad67c8e2f7dce9f44f
Beata <beata.kazmierczak13@gmail.com>
2022-04-22 19:29:01 +0200

    Initial commit.

---
+++ src/main/java/com/wsb/library2/LibraryController.java
@@ -0,0 +1,13 @@
+package com.wsb.library2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class LibraryController {
+    @GetMapping("/")
+    public String root()
+    {
+        return "root";
+    }
+}
