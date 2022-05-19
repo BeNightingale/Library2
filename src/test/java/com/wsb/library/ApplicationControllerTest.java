8641a5e79c9a04cf7df6931ce6f9bd2793183c68
Beata <beata.kazmierczak13@gmail.com>
2022-05-19 19:02:02 +0200

    tests

--- src/test/java/com/wsb/library/ApplicationControllerTest.java
+++ src/test/java/com/wsb/library/ApplicationControllerTest.java
@@ -4,10 +4,9 @@ import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class Library2ApplicationTests {
+public class ApplicationControllerTest {
 
     @Test
-    void contextLoads() {
+    void a() {
     }
-
 }
---
+++ src/test/java/com/wsb/library/service/LoanServiceTest.java
@@ -0,0 +1,12 @@
+package com.wsb.library.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class LoanServiceTest {
+
+    @Test
+    void a() {
+    }
+}
