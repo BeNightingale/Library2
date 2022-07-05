a1837aa4cb7e655d7eb82a4f2f7316d1320c80e8
Beata <beata.kazmierczak13@gmail.com>
2022-07-05 14:23:45 +0200

    Add book update and return

--- src/main/java/com/wsb/library/LibraryController.java
+++ src/main/java/com/wsb/library/LibraryController.java
@@ -26,14 +26,30 @@ public class LibraryController {
         return bookService.getBook(bookId).orElse(null);
     }
 
-   @PostMapping("/book")
-   public Book addABook(@RequestBody Book book) {
+    @PostMapping("/book")
+    public Book addABook(@RequestBody Book book) {
        return bookService.addBook(book.getTitle(),
                                 book.getAuthor(),
                                 book.getIsbn());
     }
+    @PutMapping("/books/{bookId}")
+    public Book updateBook(@PathVariable int bookId,
+                               @RequestParam String title,
+                               @RequestParam String author,
+                               @RequestParam Long isbn) {
+        return bookService.updateBook(bookId, title, author, isbn).orElse(null);
+    }
+    @PutMapping("/books/return/{bookId}")
+    public String returnBook(@PathVariable int bookId) {
+        if (bookService.getBook(bookId).get().isBorrowed()) {
+            bookService.returnBook(bookId);
+            return "Ok, returned!";
+        } else {
+            return "The book wasn't borrowed, it is available!";
+        }
+    }
 
-   @PostMapping("/reader/arg")
+    @PostMapping("/reader/arg")
     public Reader addAReader1(@RequestParam String firstName,
                             @RequestParam String lastName,
                             @RequestParam String address,
